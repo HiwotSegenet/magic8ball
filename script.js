d6d29013a1a345f16e568ba0fba6f437481075e2
Hiwot Segenet <74459219+HiwotSegenet@users.noreply.github.com>
2021-10-28 19:27:07 -0400

    last project

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes - definitley", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to  yes", "Don't count on it","My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"];
+var answers = ["Absolutley!", "Try again later", "Yes", "No", "My reply is no", "In your dreams", "It is certain", "May be later"];
 
 window.onload = function() {
     var eight = document.getElementById("eight");
     var answer = document.getElementById("answer");
-    var magicball = document.getElementById("magic-ball");
+    var ask = document.getElementById("magic-ball");
     var question = document.getElementById("question");
 
 
-    magicball.addEventListener("click", function() {
+    ask.addEventListener("click", function() {
         if (question.value.length < 1) {
             alert("Please enter a question!");
         } else {
